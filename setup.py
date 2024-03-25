701304e0410b9d34dd465607e26240bc883554ca
joanne-affolter <113984234+joanne-affolter@users.noreply.github.com>
2024-03-25 15:47:40 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyplutchik',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your project's dependencies here.
+        # e.g., 'requests >= 2.19.1',
+    ],
+    # Include other arguments as needed
+)
