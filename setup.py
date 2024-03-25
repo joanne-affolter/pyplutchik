39c83c2e98de422d1ec1fece1685dad7c397aed1
joanne-affolter <113984234+joanne-affolter@users.noreply.github.com>
2024-03-25 15:56:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        Shapely==1.7.1
-        numpy==1.17.4
-        matplotlib==3.3.2
-        descartes==1.1.0
+        'Shapely==1.7.1',
+        'numpy==1.17.4',
+        'matplotlib==3.3.2',
+        'descartes==1.1.0'
     ],
     # Include other arguments as needed
 )
